e0e2d4e3a23e4a6fb62795463544c28fa2674096
alanps <alanps3699@gmail.com>
2026-02-22 17:11:53 +0530

    added datecalculation method

--- Server.java
+++ Server.java
@@ -4,10 +4,15 @@ import com.sun.net.httpserver.HttpServer;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.temporal.ChronoUnit;
 
 public class Server {
+    static int x=0;
     public static void main(String[] args)
     {
+        
         try{
             int port = System.getenv("PORT")!=null? Integer.parseInt(System.getenv("PORT")):8080;
             HttpServer server = HttpServer.create(new InetSocketAddress(port),0);
@@ -21,15 +26,25 @@ public class Server {
             System.out.println(e.getMessage());
         }
     }
-
+    
     static class MyHandler implements HttpHandler{
         @Override
         public void handle(HttpExchange exchange) throws IOException{
-            String response = "Hello, this is an example for my new webserver";
+            long days = CalculateDuration();
+            String response = days+" Days Left for Exam \n Site visited "+x+" times";
             exchange.sendResponseHeaders(200, response.length());
             OutputStream os = exchange.getResponseBody();
             os.write(response.getBytes());
             os.close();
         }
+    }
+    static long CalculateDuration(){
+        x++;
+        LocalDate dateBefore = LocalDate.now();
+        LocalDate dateAfter = LocalDate.of(2026,Month.JUNE,15);
+        long daysBetween = ChronoUnit.DAYS.between(dateBefore, dateAfter);
+        System.out.println(daysBetween);
+
+        return daysBetween;
     }
 }
